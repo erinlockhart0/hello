82d013950b117bb5e75af34139c288c6f09b4bbb
Erin Lockhart <elockhart4@huskers.unl.edu>
2020-08-16 12:25:57 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Erin Lockhart
+ *         elockhart4@huskers.unl.edu
+ * Date: 2020/08/15
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Erin Lockhart!\n");
+  printf ("Neuroscience\n");
+
+  return 0;
+}
